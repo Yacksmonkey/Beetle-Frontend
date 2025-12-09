4b3d58fbbe5596ebb66429855e93eac2db888de0
Horacio Rodriguez <96556399+AngelRRand@users.noreply.github.com>
2025-12-09 09:28:38 -0300

    BeetleApp resolve deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import {ThemeProvider} from "next-themes";
+import Navbar from "@/components/core/navbar";
+import Footer from "@/components/core/footer";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -29,7 +31,7 @@ export default function RootLayout({
 		<body
 			className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 		>
-
+		<Navbar/>
 		<ThemeProvider
 			attribute="class"
 			defaultTheme="system"
@@ -38,6 +40,7 @@ export default function RootLayout({
 		>
 			{children}
 		</ThemeProvider>
+		<Footer/>
 		</body>
 		</html>
 	);
